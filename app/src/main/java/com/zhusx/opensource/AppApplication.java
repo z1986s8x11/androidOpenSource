7ac02e5842b17b781cd0f8896e62156df2f57726
z1986s8x11 <z1986s8x11@163.com>
2018-08-16 15:11:50 +0800

    init

---
+++ app/src/main/java/com/zhusx/opensource/AppApplication.java
@@ -0,0 +1,20 @@
+package com.zhusx.opensource;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.zhusx.core.ZsxApplicationManager;
+
+/**
+ * Author  zhusx
+ * Email   327270607@qq.com
+ * Create  2018/8/14 9:36
+ */
+public class AppApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ZsxApplicationManager.builder(this).setLogDebug(BuildConfig.DEBUG).build();
+        Fresco.initialize(this);
+    }
+}
