ebd0776ef8722f83ffa4fd54d4996fdad384d838
z1986s8x11 <z1986s8x11@163.com>
2019-02-28 16:25:09 +0800

    init

--- app/src/main/java/com/zhusx/opensource/progress/WhiteProgressViewActivity.kt
+++ app/src/main/java/com/zhusx/opensource/progress/WhiteProgressViewActivity.kt
@@ -3,6 +3,7 @@ package com.zhusx.opensource.progress
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.zhusx.opensource.R
+import kotlinx.android.synthetic.main.activity_white_progress.*
 
 /**
  *
@@ -10,9 +11,10 @@ import com.zhusx.opensource.R
  * Email   327270607@qq.com
  * Create  2019/2/28 10:53
  */
-class WhiteProgressViewActivity: AppCompatActivity(){
+class WhiteProgressViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_white_progress)
+        circle_progress_normal.setProgressInTime(0,68, 2500)
     }
 }
