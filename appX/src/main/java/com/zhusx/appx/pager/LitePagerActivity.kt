3be668bace3d1c7786f4a0c9cff7dbe04b3ec039
zhusx <z1986s8x11@163.com>
2019-08-08 09:54:27 +0800

    init

--- appX/src/main/java/com/zhusx/appx/pager/LitePagerActivity.kt
+++ appX/src/main/java/com/zhusx/appx/pager/LitePagerActivity.kt
@@ -1,7 +1,7 @@
 package com.zhusx.appx.pager
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.zhusx.appx.R
 
 /**
